6c02e8a10af2cda2a415e2a5f6e8b5ceb2d93920
hegde-atri <iamatrihegde@gmail.com>
2022-02-26 11:41:41 +0000

    Initial commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using ReactiveUI;
+
+namespace FileOrganizer.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public int count;
+
+        public int Count
+        {
+            get => count;
+            set => this.RaiseAndSetIfChanged(ref count, value);
+        }
+        
+        public ICommand AddOneCommand { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            AddOneCommand = ReactiveCommand.Create(() => { Count++;});
+        }
+    }
+}
