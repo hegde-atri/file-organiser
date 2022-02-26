89b1f362897870e49a1517990436be1a9afd5c04
hegde-atri <iamatrihegde@gmail.com>
2022-02-26 15:43:18 +0000

    ui

---
+++ README.md
@@ -0,0 +1,7 @@
+# File Organizer
+I made this project that organizes files in a folder based on specified options.
+This was made to organize recovered data from my hard drive. Suitable for photos, videos and word documents.
+
+## Options
+ - By Created Date.
+ - By File type.
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Input;
+using Microsoft.CodeAnalysis.Text;
 using ReactiveUI;
 
 namespace FileOrganizer.ViewModels
@@ -9,18 +12,102 @@ namespace FileOrganizer.ViewModels
     public class MainWindowViewModel : ViewModelBase
     {
         public int count;
+        public string path;
+        public string error;
+        public string fileType;
+        public float progress;
+        public string output;
 
         public int Count
         {
             get => count;
             set => this.RaiseAndSetIfChanged(ref count, value);
         }
+
+        public string Path
+        {
+            get => path;
+            set => this.RaiseAndSetIfChanged(ref path, value);
+        }
+
+        public string Error
+        {
+            get => error;
+            set => this.RaiseAndSetIfChanged(ref error, value);
+        }
+
+        public string FileType
+        {
+            get => fileType;
+            set => this.RaiseAndSetIfChanged(ref fileType, value);
+        }
+
+        public float Progress
+        {
+            get => progress;
+            set => this.RaiseAndSetIfChanged(ref progress, value);
+        }
         
         public ICommand AddOneCommand { get; private set; }
+        public ICommand Organize { get; private set; }
 
         public MainWindowViewModel()
         {
-            AddOneCommand = ReactiveCommand.Create(() => { Count++;});
+            AddOneCommand = ReactiveCommand.Create(() =>
+            {
+                Count++;
+            });
+
+            Organize = ReactiveCommand.Create(async () =>
+            {
+                if (optionsValid())
+                {
+                    if (CheckPathValidity())
+                    {
+                        Progress = 10;
+                        await Task.Delay(TimeSpan.FromSeconds(1));
+                        Progress = 20;
+                        await Task.Delay(TimeSpan.FromSeconds(1));
+                        Progress = 30;
+                        await Task.Delay(TimeSpan.FromSeconds(1));
+                        Progress = 40;
+                        await Task.Delay(TimeSpan.FromSeconds(1));
+                        Progress = 50;
+                        await Task.Delay(TimeSpan.FromSeconds(1));
+                        Progress = 60;
+                        await Task.Delay(TimeSpan.FromSeconds(1));
+                        Progress = 70;
+                        await Task.Delay(TimeSpan.FromSeconds(1));
+                        Progress = 80;
+                        await Task.Delay(TimeSpan.FromSeconds(1));
+                        Progress = 90;
+                        await Task.Delay(TimeSpan.FromSeconds(1));
+                        Progress = 100;
+                        await Task.Delay(TimeSpan.FromSeconds(1));
+                        
+                    }
+                }
+                
+            });
+
+
+        }
+
+        public bool CheckPathValidity()
+        {
+            // Error = "File path invalid!";
+            return true;
+        }
+
+        public bool optionsValid()
+        {
+            
+            return true;
+        }
+
+        public void updateProgress()
+        {
+            
         }
     }
 }
