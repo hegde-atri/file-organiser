3468f27343e8456bb95ed8c9617fb8a0b6e59fc3
hegde-atri <iamatrihegde@outlook.com>
2023-07-02 12:44:51 +0100

    feat: started

--- src/file_organiser.rs
+++
@@ -1,9 +0,0 @@
-pub struct FileOrganiser {}
-
-impl FileOrganiser {}
-
-impl Application for FileOrganiser {
-  fn new(_flags: ()) -> (Self, Command<Self::Message>){
-
-  }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,110 +1,95 @@
-use iced::{
-    theme,
-    widget::{button, horizontal_space, row},
-    Color, Element, Sandbox, Settings,
-};
-mod gui;
-mod lib;
+#![windows_subsystem = "windows"]
+
+use std::fmt::Error;
+use std::path::PathBuf;
+
+use iced::alignment;
+use iced::executor;
+use iced::theme;
+use iced::widget::{checkbox, column, container, Text};
+use iced::{Application, Command, Element, Length, Settings, Theme};
 
 fn main() -> iced::Result {
-    FileOrganiser::run(Settings::default())
+    Example::run(Settings::default())
 }
 
-pub struct FileOrganiser {
-    steps: Steps,
+#[derive(Default)]
+struct Example {
+    src_path: String,
+    dest_path: String,
+    file_type: String,
+    recursive: bool,
+    progress: f32,
+    organising: bool,
 }
 
-impl Sandbox for FileOrganiser {
+#[derive(Debug, Clone)]
+enum Message {
+    SrcPathChanged(Result<PathBuf, Error>),
+    DestPathChanged(Result<PathBuf, Error>),
+    FileTypeChanged(String),
+    RecursiveChanged(),
+    ProgressChanged(),
+}
+
+impl Application for Example {
     type Message = Message;
+    type Flags = ();
+    type Executor = executor::Default;
+    type Theme = Theme;
 
-    fn new() -> FileOrganiser {
-        FileOrganiser {
-            steps: Steps::new(),
-        }
+    fn new(_flags: ()) -> (Self, Command<Message>) {
+        (
+            Self {
+                src_path: String::from(""),
+                dest_path: String::from(""),
+                file_type: String::from(""),
+                recursive: false,
+                progress: 0.0,
+                organising: false,
+            },
+            Command::none(),
+        )
     }
 
     fn title(&self) -> String {
-        format!("{} - Iced", self.steps.title())
+        String::from("File Organiser")
     }
 
-    fn update(&mut self, event: Message) {
-        match event {
-            Message::BackPressed => {
-                self.steps.go_back();
+    fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
+        match message {
+            Message::SrcPathChanged(res) => {
+                self.src_path = String::from("src path changed");
             }
-            Message::NextPressed => {
-                self.steps.advance();
+            Message::DestPathChanged(res) => {
+                self.dest_path = String::from("dest path changed");
             }
-            Message::StepMessage(step_msg) => {
-                self.steps.update(step_msg);
+            Message::FileTypeChanged(file_type) => {
+                self.file_type = String::from("file type changed");
+            }
+            Message::RecursiveChanged() => {
+                self.recursive = !self.recursive;
+            }
+            Message::ProgressChanged() => {
+                self.progress += 0.1;
             }
         }
+        Command::none()
     }
 
-    fn view(&self) -> Element<Message> {
-        let FileOrganiser { steps } = self;
-        let mut controls = row![];
+    fn view(&self) -> Element<'_, Self::Message, iced::Renderer<Self::Theme>> {
+        let heading: Text = Text::new("AGM Price Calculator").size(50);
 
-        if steps.has_previous() {
-            controls = controls.push(
-                button("Back")
-                    .on_press(Message::BackPressed)
-                    .style(theme::Button::Secondary),
-            );
-        }
+        let mut contents = column![];
 
-        controls = controls.push(horizontal_space(Length::Fill));
+        contents = contents.push(heading);
 
-        if steps.can_continue() {
-            controls = controls.push(
-                button("Next")
-                    .on_press(Message::NextPressed)
-                    .style(theme::Button::Primary),
-            );
-        }
-
-        // FIXME
-        // let content: Element<_> =
-        // column![steps.view(self.debug).map(Message::StepMessage), controls,]
-        //     .max_width(540)
-        //     .spacing(20)
-        //     .padding(20)
-        //     .into();
+        container(contents.spacing(20).padding(20))
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .padding(20)
+            .align_y(alignment::Vertical::Center)
+            .align_x(alignment::Horizontal::Center)
+            .into()
     }
-}
-
-struct Steps {
-    steps: Vec<Step>,
-    current: usize,
-}
-
-impl Steps {
-    // FIXME
-    fn new() -> Steps {}
-}
-
-enum Step {
-    Welcome,
-    ChooseDirs,
-    ChooseOptions,
-    Progress,
-    End,
-}
-
-#[derive(Debug, Clone)]
-pub enum Message {
-    BackPressed,
-    NextPressed,
-    StepMessage(StepMessage),
-}
-
-#[derive(Debug, Clone)]
-pub enum StepMessage {
-    SliderChanged(u8),
-    SpacingChanged(u16),
-    TextSizeChanged(u16),
-    TextColorChanged(Color),
-    ImageWidthChanged(u16),
-    InputChanged(String),
-    TogglerChanged(bool),
 }
